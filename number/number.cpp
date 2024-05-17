9266144bfd444f6fc2552e4124b99567d8681cf9
Виталя <@DESKTOP-3KLFUBO>
2024-05-17 22:19:32 +1000

    Добавьте файлы проекта.

---
+++ number/number.cpp
@@ -0,0 +1,10 @@
+﻿//Изменения в программе(добавлен вывод числа 42) :
+#include <iostream>
+using  namespace std;
+int main() {
+    setlocale(0, "RUSS");
+    cout << "Hello, world!" << endl;
+    cout << "number: 42" << endl;
+    return 0;
+}
+//Ссылка на обновленный репозиторий : https:
